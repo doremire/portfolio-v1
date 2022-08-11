213cf40ccc836c5bbb014683c4f322a27ac8c316
Doremire <86954291+NICu0@users.noreply.github.com>
2022-08-12 06:11:43 +0900

    commit

--- js/index.js
+++ js/index.js
@@ -14,16 +14,16 @@ ScrollReveal().reveal('.item', { reset: true, distance: '40px' });
 const check = document.getElementById("nav-input");
 
 function checkbox0() {
-    window.scrollTo(0, 700);
+    window.scrollTo(0, 600);
     check.checked = false;
 }
 
 function checkbox1() {
-    window.scrollTo(0, 1300);
+    window.scrollTo(0, 1200);
     check.checked = false;
 }
 
 function checkbox2() {
-    window.scrollTo(0, 2010);
+    window.scrollTo(0, 1840);
     check.checked = false;
 }
